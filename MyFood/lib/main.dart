abf1bf96b6e5acb8e7ebd56be4b8d14669eb3679
mikemertens <mikemertens15@gmail.com>
2020-09-03 12:07:58 -0400

    Made skeleton layout for registration

--- MyFood/lib/main.dart
+++ MyFood/lib/main.dart
@@ -1,66 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyFood());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyFood extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'MyFood'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("MyFood"),
+        ),
+        body: Center(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/logo_MyFood.png'),
+                SizedBox(height: 10),
+                Text("Name Field Here", style: TextStyle(fontSize: 25)),
+                SizedBox(height: 10),
+                Text("Email Field Here", style: TextStyle(fontSize: 25)),
+                SizedBox(height: 10),
+                Text("Password Field Here", style: TextStyle(fontSize: 25)),
+                SizedBox(height: 10),
+                Text("Confirm Password Field Here", style: TextStyle(fontSize: 25)),
+                SizedBox(height: 20),
+                Text("Register Button Here", style: TextStyle(fontSize: 35))
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+            height: 500,
+            width: 500,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
