91fea594479c6ec907cabd11b6afd1b54f07e6c3
rmarh001 <rmarh001@plattsburgh.edu>
2020-09-15 11:22:54 -0400

    Made home screen a separate page and touched up main code to get rid of debug banner and changed background color

--- MyFood/lib/main.dart
+++ MyFood/lib/main.dart
@@ -1,41 +1,20 @@
 import 'package:flutter/material.dart';
+import './view/MainPage.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyFood());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyFood extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyFood App',
       debugShowCheckedModeBanner: false,
       theme: new ThemeData(
         scaffoldBackgroundColor: const Color(0xFFA4E5B5),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'MyFood'),
+      home: MainPage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: IconButton(
-      icon: Image.asset('assets/user.png'),
-      iconSize: 50,
-      onPressed: () {},
-    ));
   }
 }
