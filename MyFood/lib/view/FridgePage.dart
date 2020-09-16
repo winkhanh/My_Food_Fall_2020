c4fa7b97910b7337525de0203471f30751810320
mikemertens15 <44930801+mikemertens15@users.noreply.github.com>
2020-09-16 18:24:05 -0400

    Create FridgePage.dart
    Add initial Fridge Page

---
+++ MyFood/lib/view/FridgePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FridgePage extends StatefulWidget {
+  FridgePage({Key key}) : super(key: key);
+
+  @override
+  _FridgePageState createState() => _FridgePageState();
+}
+
+class _FridgePageState extends State<FridgePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
