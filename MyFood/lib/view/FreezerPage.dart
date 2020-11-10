7919fb9843abc99f04273a70f5ce2ff71e855564
rmarh001 <rmarh001@plattsburgh.edu>
2020-11-10 11:11:12 -0500

    working freezer page

--- MyFood/lib/view/FreezerPage.dart
+++ MyFood/lib/view/FreezerPage.dart
@@ -14,6 +14,18 @@ class _FreezerPageState extends State<FreezerPage> {
   //FirebaseFirestore db = FirebaseFirestore.getInstance();
   TextEditingController _textController = TextEditingController();
 
+  Future getPosts() async {
+    var db = FirebaseFirestore.instance;
+
+    QuerySnapshot qn = await db
+        .collection("Users")
+        .doc("Username02")
+        .collection("Drawer")
+        .get();
+
+    return qn.docs;
+  }
+
   List<int> amount = <int>[/*2, 0, 10, 6, 52, 4, 0, 2, 1, 2, 3, 4, 5, 6, 7*/];
   List<String> foodItem = <String>[];
 
@@ -31,6 +43,23 @@ class _FreezerPageState extends State<FreezerPage> {
     });
   }
 
+  /* public void getMultipleDocs(){
+    FirebaseFirestore.instance
+          .collection("Users")
+          .doc("Username02")
+          .collection("Drawer").whereEqualTo("Type", "Freezer").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -132,64 +161,45 @@ class _FreezerPageState extends State<FreezerPage> {
                 Row(
                   children: [
                     Container(
-                      child: Padding(
-                          padding: EdgeInsets.only(left: 26.5, top: 49),
-                          child: Container(
-                            child: ListView.builder(
-                                padding: const EdgeInsets.all(8),
-                                itemCount: foodItem.length,
-                                itemBuilder: (BuildContext context, int index) {
-                                  return Dismissible(
-                                      key: Key(foodItem[index]),
-                                      onDismissed: (direction) {
-                                        foodItem.removeAt(index);
-                                        amount.removeAt(index);
-                                      },
-                                      child: Container(
-                                        height: 50,
-                                        margin: EdgeInsets.all(2),
-                                        color: Colors.white,
-                                        child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Expanded(
-                                                  child: SizedBox(
-                                                      child: StreamBuilder(
-                                                stream: FirebaseFirestore
-                                                    .instance
-                                                    .collection("Users")
-                                                    .doc("Username02")
-                                                    .collection("Drawer")
-                                                    .snapshots(),
-                                                builder: (BuildContext context,
-                                                    AsyncSnapshot<QuerySnapshot>
-                                                        snapshot) {
-                                                  return ListView(
-                                                    children: snapshot.data.docs
-                                                        .map((document) {
-                                                      return Container(
-                                                          child: Text(document[
-                                                              "Name"]));
-                                                    }).toList(),
-                                                  );
-                                                },
-                                              )))
-                                            ]),
-                                      ));
-                                }),
-                            width: 353,
-                            height: 481,
-                            decoration: BoxDecoration(
-                                //color: Color(0xff3f6576),
-                                color: Colors.blue[200],
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(15),
-                                  topRight: Radius.circular(15),
-                                  bottomRight: Radius.circular(15),
-                                )),
+                        child: Padding(
+                      padding: EdgeInsets.only(left: 26.5, top: 49),
+                      child: Container(
+                          width: 353,
+                          height: 481,
+                          decoration: BoxDecoration(
+                              //color: Color(0xff3f6576),
+                              color: Colors.blue[200],
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(15),
+                                topRight: Radius.circular(15),
+                                bottomRight: Radius.circular(15),
+                              )),
+                          child: FutureBuilder(
+                            future: getPosts(),
+                            builder: (_, snapshot) {
+                              print("testing");
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return Center(
+                                  child: Text("Loading..."),
+                                );
+                              } else {
+                                for (var item in snapshot.data) {
+                                  print(item.data);
+                                }
+                                return ListView.builder(
+                                    itemCount: snapshot.data.length,
+                                    itemBuilder: (_, index) {
+                                      print(snapshot.data[0].get("Name"));
+                                      return ListTile(
+                                        title: Text(
+                                            snapshot.data[index].get("Name")),
+                                      );
+                                    });
+                              }
+                            },
                           )),
-                    )
+                    ))
                   ],
                 ),
               ],
